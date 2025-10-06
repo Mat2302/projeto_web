ecee209daa5f0455d49eaa41e5eb09bbfbb232a1
laurusso <lauurussoo@gmail.com>
2025-10-05 21:33:03 -0300

    Tentativa de header dinamico nao finalizada

---
+++ front_end/src/header.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var header = document.querySelector(".header-container");
+
+    if(!header) {
+        console.error("Div 'header-container' não encontrada!");
+        return;
+    }
+
+    fetch("../components/header.html")
+    .then(response =>  {
+        if(!response.ok) { //http 
+            throw new Error("Arquivo não encontrado! status: " + response.status);
+        }
+        return response.text();
+    })
+
+    .then(html => {
+        header.innerHTML = html;
+    })
+
+    .catch(error => {
+        console.error("Falha ao carregar o header: ", error);
+    });
+});
